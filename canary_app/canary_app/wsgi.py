d733f3ba80b17241c724b94019ac85635868dffb
Steven Holmes <steven.w.holmes@gmail.com>
2017-03-10 13:38:45 -0600

    reconfigured structure

--- canary_app/canary_app/wsgi.py
+++ canary_app/canary_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for canary project.
+WSGI config for canary_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "canary.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "canary_app.settings")
 
 application = get_wsgi_application()
