a4a740394b28f61358b0ac12bf5c660c0b062e10
Steven Holmes <steven.holmes@juiceanalytics.com>
2019-02-12 21:57:14 -0500

    use all tweets including retweets

--- canary_app/canary/streamer.py
+++ canary_app/canary/streamer.py
@@ -1,21 +1,24 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
-from canary.processor import *
+from processor import *
 
 processor = Processor()
+
 
 class Listener(StreamListener):
 
     def on_status(self, status):
         # Get tweets -> exclude retweets
-        if 'RT @' in status.text:
-            return
-        else:
-            processor.process_tweets(status)
-            return True
+        print status
+        processor.process_tweets(status)
+        # if 'RT @' in status.text:
+        #     return
+        # else:
+        #     processor.process_tweets(status)
+        #     return True
 
     def on_error(self, status_code):
         # Error handling
         if status_code == 420:
             print('Error: Status Code 420')
-            return True
+            return False
