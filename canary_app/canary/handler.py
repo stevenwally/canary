c76cc9e19257c0d5ffd77caf944faeb2d55b5c87
Steven Holmes <steven.holmes@juiceanalytics.com>
2019-02-16 16:06:31 -0500

    Create module for Sentiment

--- canary_app/canary/handler.py
+++ canary_app/canary/handler.py
@@ -1,48 +1,4 @@
-
-class Sentiment(object):
-
-    def __init__(self):
-        self.sentiment_value = 0
-        self.tweets = []
-        self.location = []
-        self.percentage = None
-        self.keyword = None
-
-    def get_sentiment_value(self):
-        return self.sentiment_value
-
-    def set_sentiment_value(self, value):
-        self.sentiment_value = value
-
-    def increment_sentiment_value(self):
-        self.sentiment_value += 1
-
-    def get_tweets(self):
-        return self.tweets
-
-    def add_tweet(self, tweet):
-        self.tweets.append(tweet)
-
-    def get_location(self):
-        return self.location
-
-    def add_location(self, location):
-        self.location.append(location)
-
-    def get_percentage(self):
-        return self.percentage
-
-    def set_percentage(self, value):
-        self.percentage = value
-
-    def get_keyword(self):
-        return self.keyword
-
-    def set_keyword(self, keyword):
-        self.keyword = keyword
-
-    def clear(self):
-        self.__init__()
+from sentiment import Sentiment
 
 
 class Handler(object):
@@ -58,13 +14,11 @@ class Handler(object):
         self.neutral.clear()
 
     def set_keyword(self, keyword):
-
         self.positive.set_keyword(keyword)
         self.negative.set_keyword(keyword)
         self.neutral.set_keyword(keyword)
 
     def set_tweet(self, text, polarity):
-
         if polarity > 0:
             self.positive.add_tweet(text)
         elif polarity < 0:
@@ -72,20 +26,7 @@ class Handler(object):
         else:
             self.neutral.add_tweet(text)
 
-    # def set_top_rt(self, text, count, polarity):
-    #
-    #     if polarity > 0:
-    #         self.positive['rt_count'] = count
-    #         self.positive['top_rt'] = text
-    #     elif polarity < 0:
-    #         self.negative['rt_count'] = count
-    #         self.negative['top_rt'] = text
-    #     else:
-    #         self.neutral['rt_count'] = count
-    #         self.neutral['top_rt'] = text
-
     def set_sentiment(self, polarity):
-
         if polarity > 0:
             self.positive.increment_sentiment_value()
         elif polarity < 0:
---
+++ canary_app/canary/sentiment.py
@@ -0,0 +1,44 @@
+class Sentiment(object):
+
+    def __init__(self):
+        self.sentiment_value = 0
+        self.tweets = []
+        self.location = []
+        self.percentage = None
+        self.keyword = None
+
+    def get_sentiment_value(self):
+        return self.sentiment_value
+
+    def set_sentiment_value(self, value):
+        self.sentiment_value = value
+
+    def increment_sentiment_value(self):
+        self.sentiment_value += 1
+
+    def get_tweets(self):
+        return self.tweets
+
+    def add_tweet(self, tweet):
+        self.tweets.append(tweet)
+
+    def get_location(self):
+        return self.location
+
+    def add_location(self, location):
+        self.location.append(location)
+
+    def get_percentage(self):
+        return self.percentage
+
+    def set_percentage(self, value):
+        self.percentage = value
+
+    def get_keyword(self):
+        return self.keyword
+
+    def set_keyword(self, keyword):
+        self.keyword = keyword
+
+    def clear(self):
+        self.__init__()
